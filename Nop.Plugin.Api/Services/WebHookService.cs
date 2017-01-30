c5e8e83be2dca0f915b366b80e62a6e99c22ac25
Ivan Stoyanov <ivanst.stoyanov@gmail.com>
2017-01-30 13:55:25 +0200

    Add the web hook ID to the web hook data

--- Nop.Plugin.Api/Services/WebHookService.cs
+++ Nop.Plugin.Api/Services/WebHookService.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNet.WebHooks;
+﻿using Microsoft.AspNet.WebHooks;
 using Microsoft.AspNet.WebHooks.Diagnostics;
 using Microsoft.AspNet.WebHooks.Services;
+using Nop.Plugin.Api.WebHooks;
 
 namespace Nop.Plugin.Api.Services
 {
@@ -19,7 +15,7 @@ namespace Nop.Plugin.Api.Services
             {
                 ILogger logger = new TraceLogger();
                 IWebHookStore store = CustomServices.GetStore();
-                IWebHookSender sender = CustomServices.GetSender(logger);
+                IWebHookSender sender = new ApiWebHookSender(logger);
 
                 _webHookManager = new WebHookManager(store, sender, logger);
             }
---
+++ Nop.Plugin.Api/WebHooks/ApiWebHookSender.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.WebHooks;
+using Microsoft.AspNet.WebHooks.Diagnostics;
+using Newtonsoft.Json.Linq;
+
+namespace Nop.Plugin.Api.WebHooks
+{
+    public class ApiWebHookSender : DataflowWebHookSender
+    {
+        private const string WebHookIdKey = "WebHookId";
+
+        public ApiWebHookSender(ILogger logger) : base(logger)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override JObject CreateWebHookRequestBody(WebHookWorkItem workItem)
+        {
+            JObject data = base.CreateWebHookRequestBody(workItem);
+
+            Dictionary<string, object> body = data.ToObject<Dictionary<string, object>>();
+
+            body[WebHookIdKey] = workItem.WebHook.Id;
+
+            return JObject.FromObject(body);
+        }
+    }
+}
