fee4986ff8f6c739de7bd82ef2b4374216259095
Ivan Stoyanov <ivanst.stoyanov@gmail.com>
2017-01-30 16:14:58 +0200

    Change the web hook names

--- Nop.Plugin.Api/Constants/WebHookNames.cs
+++ Nop.Plugin.Api/Constants/WebHookNames.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Nop.Plugin.Api.Constants
+﻿namespace Nop.Plugin.Api.Constants
 {
     public static class WebHookNames
     {
@@ -12,20 +6,20 @@ namespace Nop.Plugin.Api.Constants
 
         public const string GetWebhookByIdAction = "GetWebHookByIdAction";
 
-        public const string CustomerCreated = "customer/created";
-        public const string CustomerUpdated = "customer/updated";
-        public const string CustomerDeleted = "customer/deleted";
+        public const string CustomerCreated = "customers/created";
+        public const string CustomerUpdated = "customers/updated";
+        public const string CustomerDeleted = "customers/deleted";
 
-        public const string ProductCreated = "product/created";
-        public const string ProductUpdated = "product/updated";
-        public const string ProductDeleted = "product/deleted";
+        public const string ProductCreated = "products/created";
+        public const string ProductUpdated = "products/updated";
+        public const string ProductDeleted = "products/deleted";
 
-        public const string CategoryCreated = "category/created";
-        public const string CategoryUpdated = "category/updated";
-        public const string CategoryDeleted = "category/deleted";
+        public const string CategoryCreated = "categories/created";
+        public const string CategoryUpdated = "categories/updated";
+        public const string CategoryDeleted = "categories/deleted";
 
-        public const string OrderCreated = "order/created";
-        public const string OrderUpdated = "order/updated";
-        public const string OrderDeleted = "order/deleted";
+        public const string OrderCreated = "orders/created";
+        public const string OrderUpdated = "orders/updated";
+        public const string OrderDeleted = "orders/deleted";
     }
 }
