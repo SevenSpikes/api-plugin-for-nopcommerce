c2ffc30e2cc01d44695755b946daa2eeb9287d7e
poyker <boyko.stanev@gmail.com>
2017-06-21 17:47:27 +0300

    Added Shopping Cart Attributes Support #37

---
+++ Nop.Plugin.Api/DTOs/ProductItemAttributeDto.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Plugin.Api.DTOs
+{
+    [JsonObject(Title = "attribute")]
+    public class ProductItemAttributeDto
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("value")]
+        public string Value { get; set; }
+    }
+}
