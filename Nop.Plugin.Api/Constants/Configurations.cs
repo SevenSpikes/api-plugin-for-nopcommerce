acc1d4e7471144c0201897d12d233b4428ba4c92
poyker <boyko.stanev@gmail.com>
2018-01-19 12:00:57 +0200

    Make it clear that the token expiration time is in seconds. Made both default times to be int.MaxValue

--- Nop.Plugin.Api/Constants/Configurations.cs
+++ Nop.Plugin.Api/Constants/Configurations.cs
@@ -6,7 +6,7 @@
         // It is recommended to keep your access token expiration time to 1 hour and to use the refresh token to obtain a new one after expiration.
         // Refresh token lifespan should be max one month. 
         // Please, edit the configuration bellow!!!
-        public const int DefaultAccessTokenExpiration = 43200 * 60;
+        public const int DefaultAccessTokenExpiration = int.MaxValue;
         public const int DefaultRefreshTokenExpiration = int.MaxValue;
         public const int DefaultLimit = 50;
         public const int DefaultPageValue = 1;
