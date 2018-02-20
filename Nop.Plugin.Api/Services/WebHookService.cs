bd37e037753de9d02d828be2b4bedbf7c313059b
Ivan Stoyanov <ivanst.stoyanov@gmail.com>
2018-02-20 13:15:43 +0200

    Fix an issue with the webhooks not being send.

--- Nop.Plugin.Api/Services/WebHookService.cs
+++ Nop.Plugin.Api/Services/WebHookService.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNet.WebHooks;
-using Microsoft.AspNet.WebHooks.Diagnostics;
+
 using Microsoft.AspNet.WebHooks.Services;
 using Nop.Plugin.Api.WebHooks;
 
@@ -8,16 +8,17 @@ namespace Nop.Plugin.Api.Services
     public class WebHookService : IWebHookService
     {
         private IWebHookManager _webHookManager;
+        private IWebHookStore _webHookStore;
 
         public IWebHookManager GetHookManager()
         {
-            if (_webHookManager == null)
+            if (_webHookManager == null || _webHookStore.GetType() != typeof(SqlWebHookStore))
             {
-                ILogger logger = new TraceLogger();
-                IWebHookStore store = CustomServices.GetStore();
+                Microsoft.AspNet.WebHooks.Diagnostics.ILogger logger = new Microsoft.AspNet.WebHooks.Diagnostics.TraceLogger();
+                _webHookStore = CustomServices.GetStore();
                 IWebHookSender sender = new ApiWebHookSender(logger);
 
-                _webHookManager = new WebHookManager(store, sender, logger);
+                _webHookManager = new WebHookManager(_webHookStore, sender, logger);
             }
 
             return _webHookManager;
