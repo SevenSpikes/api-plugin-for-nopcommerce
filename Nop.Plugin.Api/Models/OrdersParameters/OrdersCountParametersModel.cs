715c9b664e79342322d0b0fb915949ad2596cdc8
MaximG1234 <32756170+MaximG1234@users.noreply.github.com>
2019-01-07 01:12:22 +1100

    Added SinceId for OrderCount in OrdersController
    Added SinceId for OrderCount in OrdersController

--- Nop.Plugin.Api/Models/OrdersParameters/OrdersCountParametersModel.cs
+++ Nop.Plugin.Api/Models/OrdersParameters/OrdersCountParametersModel.cs
@@ -3,10 +3,21 @@
 namespace Nop.Plugin.Api.Models.OrdersParameters
 {
     using Microsoft.AspNetCore.Mvc;
+    using Newtonsoft.Json;
+    using Nop.Plugin.Api.Constants;
 
     [ModelBinder(typeof(ParametersModelBinder<OrdersCountParametersModel>))]
     public class OrdersCountParametersModel : BaseOrdersParametersModel
     {
-        // Nothing special here, created just for clarity.
+        public OrdersCountParametersModel()
+        {
+            SinceId = Configurations.DefaultSinceId;
+        }
+
+        /// <summary>
+        /// Restrict results to after the specified ID
+        /// </summary>
+        [JsonProperty("since_id")]
+        public int SinceId { get; set; }
     }
 }
--- Nop.Plugin.Api/Services/OrderApiService.cs
+++ Nop.Plugin.Api/Services/OrderApiService.cs
@@ -52,11 +52,16 @@ namespace Nop.Plugin.Api.Services
             return _orderRepository.Table.FirstOrDefault(order => order.Id == orderId && !order.Deleted);
         }
 
-        public int GetOrdersCount(DateTime? createdAtMin = null, DateTime? createdAtMax = null, OrderStatus? status = null,
+        public int GetOrdersCount(DateTime? createdAtMin = null, DateTime? createdAtMax = null, OrderStatus? status = null, 
                                  PaymentStatus? paymentStatus = null, ShippingStatus? shippingStatus = null,
-                                 int? customerId = null, int? storeId = null)
+                                 int? customerId = null, int? storeId = null, int sinceId = Configurations.DefaultSinceId)
         {
             var query = GetOrdersQuery(createdAtMin, createdAtMax, status, paymentStatus, shippingStatus, customerId: customerId, storeId: storeId);
+
+            if (sinceId > 0)
+            {
+                query = query.Where(order => order.Id > sinceId);
+            }
 
             return query.Count();
         }
